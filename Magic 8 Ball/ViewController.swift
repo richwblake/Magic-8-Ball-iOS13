63d2f1ef76ff833d7a6b0ecf83c08f7b4dbff40c
wills blake <richwblake@gmail.com>
2022-03-29 19:00:18 -0700

    Done

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,7 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    
+    @IBOutlet weak var ballImageView: UIImageView!
+    
+    @IBAction func shakeBall(_ sender: UIButton) {
+        let ballArray = [UIImage(named: "ball1"),
+                         UIImage(named: "ball2"),
+                         UIImage(named: "ball3"),
+                         UIImage(named: "ball4"),
+                         UIImage(named: "ball5")]
+        
+        let randomIndex = Int.random(in: 0..<5)
+        
+        ballImageView.image = ballArray[randomIndex]
+    }
+   
 
 
 
